884a46ad9d4560649cdf7c6dc5a9ea661047a82a
AriaieBOY <ariaieboy.ir@gmail.com>
2024-12-29 15:16:30 +0330

    make spotify api url configurable

--- src/SpotifyRequest.php
+++ src/SpotifyRequest.php
@@ -8,13 +8,14 @@ use SpotifyClient;
 
 class SpotifyRequest
 {
-    private const SPOTIFY_API_URL = 'https://api.spotify.com/v1';
+    private $spotify_api_url;
 
     private $accessToken;
 
     public function __construct(string $accessToken)
     {
         $this->accessToken = $accessToken;
+        $this->spotify_api_url = config('spotify.api_url','https://api.spotify.com/v1');
     }
 
     /**
@@ -25,7 +26,7 @@ class SpotifyRequest
     public function get(string $endpoint, array $params = []): array
     {
         try {
-            $response = SpotifyClient::get(self::SPOTIFY_API_URL.$endpoint.'?'.http_build_query($params), [
+            $response = SpotifyClient::get($this->spotify_api_url.$endpoint.'?'.http_build_query($params), [
                 'headers' => [
                     'Content-Type' => 'application/json',
                     'Accepts' => 'application/json',
